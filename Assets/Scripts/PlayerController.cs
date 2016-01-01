241c0189643da6663da61cbd876a523568d1fb59
murdochd <murdochd@msoe.edu>
2016-01-01 00:34:46 -0600

    first commit - step 6 of tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Coin")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	//use FixedUpdate instead of Update for physics
+	void FixedUpdate ()
+	{
+		var xMovement = Input.GetAxis ("Horizontal");
+		var zMovement = Input.GetAxis ("Vertical");
+		var move = new Vector3 (xMovement, 0, zMovement);
+		rb.AddForce (move * speed);
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (90, 0, 0) * Time.deltaTime);
+	}
+}
