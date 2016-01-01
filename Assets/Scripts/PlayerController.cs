80c8715227693842203eedefc415cf214f6d8f9b
unknown <drew@weazie.com>
2016-01-01 02:35:35 -0600

    added scoring and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 	private Rigidbody rb;
 	public float speed;
+	public float jumpSpeed;
+	public Transform groundCheck;
+	public Text scoreText;
+	public Text winText;
+	private int score;
+	public const int numCoins = 3;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+		SetScore(0);
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.gameObject.CompareTag ("Coin")) {
+		if (other.gameObject.CompareTag ("Coin"))
+		{
 			other.gameObject.SetActive (false);
+			SetScore (++score);
 		}
 	}
 
@@ -25,5 +35,25 @@ public class PlayerController : MonoBehaviour
 		var zMovement = Input.GetAxis ("Vertical");
 		var move = new Vector3 (xMovement, 0, zMovement);
 		rb.AddForce (move * speed);
+
+		var jump = Input.GetKey (KeyCode.Space);
+		if (jump && Physics.Linecast(transform.position, groundCheck.position))
+		{
+			rb.AddForce (0, jumpSpeed, 0);
+		}
+	}
+
+	void SetScore(int newScore)
+	{
+		score = newScore;
+		scoreText.text = "Score: " + score;
+		if (score == 0)
+		{
+			winText.text = "";
+		}
+		if (score >= numCoins)
+		{
+			winText.text = "You Win!!";
+		}
 	}
 }
